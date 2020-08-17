5a17d245d9aacd1fc2d1217bee3f441f7d4ce6be
AntonoVlad <65464656+AntonoVlad@users.noreply.github.com>
2020-08-17 21:05:07 +0300

    refactor

--- main.js
+++ main.js
@@ -3,18 +3,38 @@ let input = document.querySelector("#task-input")
 let taskBlock = document.querySelector("#task-block")
 btn.onclick = function() {
     let inputValue = input.value
+    let task = createTask()
+    let checkBox = creatCheckBox()
+    task.append(checkBox)
+    let taskText = createTaskText(inputValue)
+    task.append(taskText)
+    let taskTime = createTaskTime()
+    task.append(taskTime)
+    taskBlock.append(task)
+}
+let createTask = function() {
     let task = document.createElement("div")
     task.classList.add("task")
-    taskBlock.append(task)
+
+    return task
+}
+let creatCheckBox = function () {
     let checkBox = document.createElement("input")
     checkBox.setAttribute("type", "checkbox")
-    task.append(checkBox)
+
+    return checkBox
+}
+let createTaskText = function(value) {
     let taskText = document.createElement("div")
     taskText.classList.add("task-text")
-    taskText.textContent = inputValue
-    task.append(taskText)
+    taskText.textContent = value
+
+    return taskText
+}
+let createTaskTime = function () {
     let taskTime = document.createElement("div")
     taskTime.classList.add("task-time")
     taskTime.textContent = new Date().toLocaleTimeString()
-    task.append(taskTime)
+
+    return taskTime
 }
